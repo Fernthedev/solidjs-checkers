562f736b1786777199adbf83195c93cc1abfbf58
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-09-17 10:38:35 -0400

    alive ping pong

--- express.ts
+++ express.ts
@@ -12,10 +12,37 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export let wsServer: ws.Server
 
+function setSocketAlive(ws: ws.WebSocket, alive: boolean) {
+  ;(ws as any).isAlive = alive
+}
+function getSocketAlive(ws: ws.WebSocket) {
+  return (ws as any).isAlive as boolean
+}
+
 async function createServer() {
   const app = express()
 
   wsServer = new ws.Server({ noServer: true })
+
+  wsServer.on("connection", (ws) => {
+    setSocketAlive(ws, true)
+    ws.on("pong", () => {
+      setSocketAlive(ws, true)
+    })
+  })
+
+  const interval = setInterval(() => {
+    wsServer.clients.forEach((ws) => {
+      if (getSocketAlive(ws) === false) return ws.terminate()
+
+      setSocketAlive(ws, false)
+      ws.ping()
+    })
+  }, 30000)
+
+  wsServer.on("close", () => {
+    clearInterval(interval)
+  })
 
   // Middleware that parses json and looks at requests where the Content-Type header matches the type option.
   app.use(express.json())
