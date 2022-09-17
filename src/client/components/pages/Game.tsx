23e85f9c75aa5cbbbb377686f95137937d4d6704
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-09-17 15:44:05 -0400

    Bring back og multiplayer

--- src/client/components/pages/Game.tsx
+++ src/client/components/pages/Game.tsx
@@ -3,9 +3,10 @@ import { LocalMultiplayer } from "../../logic/local_multiplayer"
 import PlayerText from "../board/player"
 import { createStore } from "solid-js/store"
 import { createEffect } from "solid-js"
-import { useNavigate } from "@solidjs/router"
+import { Link, useNavigate } from "@solidjs/router"
 import { IMultiplayerCore } from "../../logic/multiplayer"
 import { NetworkMultiplayer } from "../../logic/network_multiplayer"
+import { playerPieces } from "../../../common/board_math"
 
 
 export default function Game() {
@@ -14,39 +15,29 @@ export default function Game() {
 
   const navigator = useNavigate()
 
-  // const [multiplayer, setMultiplayer] = createStore<Readonly<IMultiplayerCore>>(
-  //   new LocalMultiplayer(width, height, [
-  //     ...player1Pieces(width, height),
-  //     ...player2Pieces(width, height),
-  //   ])
-  // )
-
-  // createEffect(() => {
-  //   const pieces = Object.values(multiplayer.getPieces())
-  //   const player1 = pieces.filter((e) => e.player === 0)
-  //   const player2 = pieces.filter((e) => e.player === 1)
-  //   if (player1.length !== 0 && player2.length !== 0) return
-
-  //   navigator(`/game_over/${player1.length === 0 ? 1 : 0}`)
-  // })
-
   const [multiplayer, setMultiplayer] = createStore<Readonly<IMultiplayerCore>>(
-    new NetworkMultiplayer(20)
+    new LocalMultiplayer(width, height, [
+      ...playerPieces(width, height, 0, false),
+      ...playerPieces(width, height, 1, true),
+    ])
   )
 
-  // createEffect(() => {
-  //   const pieces = Object.values(multiplayer.getPieces())
-  //   const player1 = pieces.filter((e) => e.player === 0)
-  //   const player2 = pieces.filter((e) => e.player === 1)
-  //   if (player1.length !== 0 && player2.length !== 0) return
+  createEffect(() => {
+    const pieces = Object.values(multiplayer.getPieces())
+    const player1 = pieces.filter((e) => e.player === 0)
+    const player2 = pieces.filter((e) => e.player === 1)
+    if (player1.length !== 0 && player2.length !== 0) return
 
-  //   navigator(`/game_over/${player1.length === 0 ? 1 : 0}`)
-  // })
+    navigator(`/game_over/${player1.length === 0 ? 1 : 0}`)
+  })
 
 
   return (
     <>
       <PlayerText multiplayer={multiplayer} />
+      <Link href={"/multiplayer_setup"} style={{ "text-align": "center" }}>
+        <h4>Online Multiplayer</h4>
+      </Link>
       <CheckerBoard width={width} height={height} multiplayer={multiplayer} />
     </>
   )
